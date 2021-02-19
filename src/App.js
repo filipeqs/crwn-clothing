ca8c0ca33a7ec621b53369514f46ae8e6c3ec1c6
Filipe Silva <filipeqs@outlook.com>
2021-02-19 08:07:34 -0500

    google signin

--- src/App.js
+++ src/App.js
@@ -6,18 +6,41 @@ import Header from './components/header/header';
 import HomePage from './pages/homepage/HomePage';
 import Shop from './pages/shop/Shop';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/SignInAndSignUpPage';
+import { auth } from './firebase/firebase';
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/shop" component={Shop} />
-                <Route path="/signin" component={SignInAndSignUpPage} />
-            </Switch>
-        </div>
-    );
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentUser: null,
+        };
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+            this.setState({ currentUser: user });
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/shop" component={Shop} />
+                    <Route path="/signin" component={SignInAndSignUpPage} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
