80b3e96b21e94d9d82b1ca14aeda78237ff521ea
Filipe Silva <filipeqs@outlook.com>
2021-02-25 11:29:41 -0500

    cart icon component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,6 +7,7 @@ import { auth } from '../../firebase/firebase';
 import './header.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/CartIcon';
 
 const Header = ({ currentUser }) => (
     <div className="header">
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             )}
+            <CartIcon />
         </div>
     </div>
 );
